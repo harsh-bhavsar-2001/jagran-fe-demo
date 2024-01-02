cd6f81650b7b074f8269459dcea1dce5ed9e9bb8
harsh-18082001 <harsh.bhavsar@saeculumsolutions.com>
2024-01-02 19:37:58 +0530

    inital commit

---
+++ src/app/[slug]/layout.jsx
@@ -0,0 +1,258 @@
+'use client';
+import '../globals.css';
+import React, { useState } from "react";
+import { useParams } from 'next/navigation'
+import axios from 'axios';
+
+
+// import '../final.css';
+
+export default function BlogDetailLayout({ children }) {
+
+  const [textValue, setTextValue] = useState("");
+  const { slug } = useParams();
+
+  const TargetList = {
+    HINDI_JAGRAN: '616a9df6-9ffa-4a13-81d8-c65ea2f92d6b',
+    ENGLISH_JAGRAN: '82d8e272-d6f2-4225-9629-4a4012a43e19',
+    JAGRAN_DEV: 'd5867ee8-34b5-4923-87b4-a7ba35ceaa06'
+  }
+
+  const onCommentClick = () => {
+    if (!textValue) {
+      alert('Please Add some comment');
+      return;
+    }
+
+    addComment();
+  }
+
+  async function addComment() {
+    try {
+      const url = `https://dev-api.jnm.digital/comment/api/v1/comment`;
+      const headers = {
+        Authorization: "Bearer " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb2RlIjoieVh1V3d0anVVYk43RkQ1ZlZBdzRHeVRNNmNTYzlBIiwidXNlcl9pZCI6IjJhZDJhOGU2LWI3ZTYtNDczYy1hZTM3LTkxM2FhZjAzM2I2MSIsInVzZXJfZW1haWwiOiJxYUB5b3BtYWlsLmNvbSJ9.3pC9tfkPNrQHE1GDYKHVC-gnWViFTyUdALIbS34cV94',
+        'X-Target': TargetList.ENGLISH_JAGRAN
+      }
+
+      const body = {
+        "comment_text": textValue,
+        "comment_article_id": slug,
+        "comment_user": {
+          "type": "manual",
+          "user_id": "8e9482388e4894d9w4",
+          "user_name": "Mark",
+          "user_email": "mark@gmail.com",
+          "user_image": "https://images.herzindagi.info/image/2022/Aug/Samridhi-Breja.jpg"
+        }
+      }
+
+      axios.post(url, body, { headers }).then(function (response) {
+        setTextValue('');
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+      }).catch(function (error) {
+        console.log(error);
+      });
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
+  const changeHandler = (event) => {
+    setTextValue(event?.target?.value);
+  }
+
+  return (
+    <>
+      <header id="mainHeader" style={{ 'zIndex': '999' }}>
+        <script src="https://www.gstatic.com/charts/loader.js"></script>
+
+
+        <link id="font-popins" rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" />
+        <link id="font-inconsolata" rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&amp;display=swap" />
+        <link id="font-mirza" rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Mirza:wght@400;500;600;700&amp;display=swap" />
+        <link id="font-merriweather" rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&amp;display=swap" />
+
+        <div className="Header_topStripContainer__XPz4A centered">
+          <div className="Header_topStripContainer__language___nE1x"><a href="#">Language</a>
+            <ul className="Header_topStripContainer__language__languageLink__YT0y6">
+              <li><a target="_blank" className="active" href="/">हिन्दी</a></li>
+              <li><a target="_blank" className="" href="https://english.jagran.com">English</a></li>
+              <li><a target="_blank" className="" href="https://www.gujaratijagran.com">ગુજરાતી</a></li>
+              <li><a target="_blank" className="" href="https://www.punjabijagran.com">ਪੰਜਾਬੀ</a></li>
+            </ul>
+          </div>
+          <ul className="Header_topStripContainer__topLink__UmZmM">
+            <li><a target="_blank" className="" href=" https://www.naidunia.com">Naidunia</a></li>
+            <li><a target="_blank" className="" href="https://www.jagranjosh.com">Jagran Josh</a></li>
+            <li><a target="_blank" className="" href="https://www.herzindagi.com">Her Zindagi</a></li>
+            <li><a target="_blank" className="" href="https://www.onlymyhealth.com">Onlymyhealth</a></li>
+            <li><a target="_blank" className="" href="https://www.vishvasnews.com">Vishvas News</a></li>
+            <li><a target="_blank" className="" href="https://www.inextlive.com">Inextlive</a></li>
+            <li><a target="_blank" className="" href="https://www.jagranplay.com">Jagran Play</a></li>
+            <li><a target="_blank" className="" href="https://www.jagrantv.com">Jagran TV</a></li>
+            <li><a target="_blank" className="" href="https://www.jagran.com/podcast">Podcast</a></li>
+          </ul>
+        </div>
+        <div className="Header_header__headerMain__AMYOZ centered">
+          <img className="header-image-new" src="https://storage.googleapis.com/dev-cms-backend-media/2023-08-10/template/image/1691651152603-aee72e92-e31e-43fa-a297-9f725f0ee3bb.png" />
+        </div>
+        <div className="centered mobilefull menuscroll">
+          <div className="stickyads mobileAds">
+            <div id="target-50" className="adsView"></div>
+          </div>
+          <div className="Header_header__mainMenu__ym_zz">
+            <ul className="Header_header__menuPrimary___Ng4k menuPrimary">
+              <li className="Header_primeImage__AN866"><a title="prime listing" href="/prime-news-hindi.html"><svg>
+                <use href="/sprite.svg#primeimages"></use>
+              </svg></a></li>
+              <li><a href="/" className="" data_id="होम">होम</a></li>
+              <li><a href="/latest-news.html?itm_medium=latest&amp;amp;itm_source=dsktp&amp;amp;itm_campaign=navigation"
+                className="" data_id="ताज़ा">ताज़ा</a></li>
+              <li><a href="/news/national-news-hindi.html?itm_medium=national&amp;amp;itm_source=dsktp&amp;amp;itm_campaign=navigation"
+                className="" data_id="राष्ट्रीय">राष्ट्रीय</a></li>
+              <li><a href="/feature-news-hindi.html" className="" data_id="एक्सप्लेनर">एक्सप्लेनर</a></li>
+              <li><a href="/specials.html?itm_medium=specials&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                className="" data_id="स्पेशल">स्पेशल</a></li>
+              <li><a href="/markets-hindi.html?utm_source=top-nav&amp;utm_medium=sharemarket&amp;utm_campaign=5paisa"
+                className="activenav" data_id="शेयर बाजार">शेयर बाजार</a></li>
+              <li><a href="/politics-news-hindi.html?itm_medium=politics&amp;amp;itm_source=dsktp&amp;amp;itm_campaign=navigation"
+                className="" data_id="पॉलिटिक्स">पॉलिटिक्स</a></li>
+              <li><a href="/world-news-hindi.html?itm_medium=world&amp;amp;itm_source=dsktp&amp;amp;itm_campaign=navigation"
+                className="" data_id="दुनिया">दुनिया</a></li>
+              <li><a href="/entertainment-hindi.html?itm_medium=entertainment&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                className="" data_id="मनोरंजन">मनोरंजन</a></li>
+              <li><a href="/business-hindi.html?itm_medium=business&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                className="" data_id="बिजनेस">बिजनेस</a></li>
+              <li><a href="/cricket-hindi.html?itm_medium=cricket&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                className="" data_id="क्रिकेट">क्रिकेट</a></li>
+              <li className="undefined dropdown"><span className="moreBtn">MORE <svg>
+                <use href="/sprite.svg#dotmenu"></use>
+              </svg></span>
+                <ul className="dropdownMenu menu Header_dropdownMenu__yE1Z4 undefined">
+                  <li><a href="/lifestyle-hindi.html?itm_medium=lifestyle&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="लाइफस्टाइल">लाइफस्टाइल</a></li>
+                  <li><a href="/technology-hindi.html?itm_medium=tech&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="टेक ज्ञान">टेक ज्ञान</a></li>
+                  <li><a href="/spiritual-hindi.html?itm_medium=spiritual&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="अध्यात्म">अध्यात्म</a></li>
+                  <li><a href="/automobile?itm_medium=auto&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="ऑटो">ऑटो</a></li>
+                  <li><a href="/news/education-news-hindi.html?itm_medium=education&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="शिक्षा">शिक्षा</a></li>
+                  <li><a href="/jokes-hindi.html?itm_medium=jokes&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="जोक्स">जोक्स</a></li>
+                  <li><a href="/common-man-issue-news-hindi.html?itm_medium=cmi&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="आम मुद्दे">आम मुद्दे</a></li>
+                  <li><a href="/news/job-news-hindi.html?itm_medium=job&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="जॉब्स">जॉब्स</a></li>
+                  <li><a href="/news/career-news-hindi.html?itm_medium=career&amp;itm_source=dsktp&amp;itm_campaign=navigation"
+                    className="" data_id="कैरियर">कैरियर</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul className="Header_header__rhsMenu__QzFiN"><a id="Prime_Subscribe" className="btn btn-primary"
+              href="https://prime.jagran.com/subscription.html">Subscribe</a><a href="#" title="searchpopup"
+                className="Header_header__search__ROsQj"><svg>
+                  <use href="/sprite.svg#search"></use>
+                </svg></a><a href="#" title="hamburger" className="Header_header__hamburger__Vinnx"><svg>
+                  <use href="/sprite.svg#menu"></use>
+                </svg></a></ul>
+          </div>
+        </div>
+
+      </header>
+      <div className="header-space"></div>
+
+      {/* <!--  BLOF POST START --> */}
+      <div className="container">
+        <div className="mainArticle">
+          <div className="main-col mainArticle__left">
+            {children}
+          </div>
+
+          <div className="mainArticle__right">
+            <div data-cx_b9kw="" className="cx-text-module">
+
+              <a id="cXLinkIdll4q80cz3ht2yvnh" href="https://www.jagran.com/cricket/headlines-kuldeep-yadav-and-tilak-varma-jumped-in-t20i-icc-ranking-tilak-varma-first-time-entered-in-rankings-t20i-23496927.html" className="cx-item cx-main" target="_top" title="WI के खिलाफ शानदार प्रदर्शन ने Tilak Varma को दिलाई ICC रैंकिंग में एंट्री, Kuldeep Yadav को भी मिला बड़ा इनाम">
+                <h3>WI के खिलाफ शानदार प्रदर्शन ने Tilak Varma को दिलाई ICC रैंकिंग में एंट्री, Kuldeep Yadav को भी मिला बड़ा इनाम</h3>
+              </a>
+
+              <a id="cXLinkIdll4q80cziui913ox" href="https://www.jagran.com/cricket/apni-baat-world-cup-2023-not-team-india-r-ashwin-picks-australia-as-favourite-for-2023-world-cup-champion-23496370.html" className="cx-item cx-main" target="_top" title="R Ashwin ने की बड़ी भविष्‍यवाणी, कहा- ये टीम बनेगी World Cup 2023 की चैंपियन, टीम इंडिया को दिया झटका">
+                <h3>R Ashwin ने की बड़ी भविष्‍यवाणी, कहा- ये टीम बनेगी World Cup 2023 की चैंपियन, टीम इंडिया को दिया झटका</h3>
+              </a>
+
+              <a id="cXLinkIdll4q80czg6wdogg6" href="https://www.jagran.com/cricket/headlines-conversation-between-hardik-pandya-and-tilak-varma-recorded-in-stump-mic-before-winning-six-in-3rd-t20i-23496582.html" className="cx-item cx-main" target="_top" title="जीत से पहले स्टंप माइक में रिकॉर्ड हुई Hardik और तिलक वर्मा की बातचीत, अब सोशल मीडिया पर तबाही मचा रहा वीडियो">
+                <h3>जीत से पहले स्टंप माइक में रिकॉर्ड हुई Hardik और तिलक वर्मा की बातचीत, अब सोशल मीडिया पर तबाही मचा रहा वीडियो</h3>
+              </a>
+
+              <a id="cXLinkIdll4q80czaftg0bwj" href="https://www.jagran.com/uttar-pradesh/varanasi-city-gyanvapi-case-past-of-the-western-wall-was-reconstructed-from-3d-images-in-gyanvapi-virtual-photographs-were-prepared-23496092.html" className="cx-item cx-main" target="_top" title="Gyanvapi Case: ज्ञानवापी में थ्रीडी इमेज से खंगाला गया पश्चिमी दीवार का अतीत, आभासी तस्वीरें की गईं तैयार">
+                <h3>Gyanvapi Case: ज्ञानवापी में थ्रीडी इमेज से खंगाला गया पश्चिमी दीवार का अतीत, आभासी तस्वीरें की गईं तैयार</h3>
+              </a>
+
+              <a id="cXLinkIdll4q80czi6zx2aen" href="https://www.jagran.com/business/biz-sebi-said-shares-of-companies-will-be-listed-in-just-3-days-after-ipo-23496690.html" className="cx-item cx-main" target="_top" title="IPO के बाद अब सिर्फ 3 दिनों में लिस्ट होंगे कंपनियों के शेयर, SEBI ने दिया बड़ा अपडेट">
+                <h3>IPO के बाद अब सिर्फ 3 दिनों में लिस्ट होंगे कंपनियों के शेयर, SEBI ने दिया बड़ा अपडेट</h3>
+              </a>
+
+              <a id="cXLinkIdll4q80czewiyyq4l" href="https://www.jagran.com/uttar-pradesh/sitapur-mother-son-electrocuted-to-death-from-mobile-charger-in-sitapur-23495443.html" className="cx-item cx-main" target="_top" title="Sitapur: आधी रात में मौत के आगोश के समा गए मां-बेटे, मोबाइल का चार्जर बना वजह; हैरान कर देगी घटना">
+                <h3>Sitapur: आधी रात में मौत के आगोश के समा गए मां-बेटे, मोबाइल का चार्जर बना वजह; हैरान कर देगी घटना</h3>
+              </a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!--  BLOF POST END --> */}
+
+      <div className="GutterBox">
+        <div className="GutterAdsLHS webAds advertiseTxt" style={{ zIndex: 9999 }}>
+          <div id="target-20" className="adsView">
+            <div id="details_lhs_160x600" data-google-query-id="COL_9ffH0YADFc72UQodeaMHaQ">
+              <div
+                className='advertisment-id'>
+                <img src='https://storage.googleapis.com/dev-cms-backend-media/2023-08-10/template/image/1691657992031-a4074bb7-8350-4c4b-97cd-d2e6c447526e.jpeg' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="GutterAdsRHS webAds advertiseTxt" style={{ zIndex: 9999 }}>
+          <div id="target-21" className="adsView">
+            <div id="details_rhs_160x600" data-google-query-id="CMLOivjH0YADFVbZ1Qod2DcIBg">
+              <div
+                className='advertisment-id'>
+
+                <img src='https://storage.googleapis.com/dev-cms-backend-media/2023-08-10/template/image/1691657992031-a4074bb7-8350-4c4b-97cd-d2e6c447526e.jpeg' />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="_15BTU container">
+        <h3 className="aKzcH">Comments</h3>
+        <form className="_2-Ckh" style={{ padding: "50px 0px" }}>
+          <img src="https://fea.assettype.com/newslaundry/assets/avatar-0992404040319c0fbe2c.png" className="ZuZCG gm-observing gm-observing-cb" style={{ padding: "8px" }} />
+          <textarea value={textValue} rows="1" onChange={changeHandler} placeholder="Enter comment" className="_1oA04"></textarea>
+          <button type="button" onClick={onCommentClick} className="_1BtHB">Send</button>
+        </form>
+      </div>
+
+      {/* <!-- FOOTER START --> */}
+      <footer>
+        <img className='footer-new-image' src="https://storage.googleapis.com/dev-cms-backend-media/2023-08-10/template/image/1691651865544-5e133e03-e13d-47f1-8bef-132602de8be5.png"
+        />
+      </footer>
+
+      <div id="snackbar">Comment added succesfully</div>
+      {/* <!-- FOOTER END --> */}
+    </>
+  )
+}
---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,141 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from 'react'
+import { useParams } from 'next/navigation'
+import { Helmet } from "react-helmet";
+// import Head from 'next/head';
+// import  Head from "next/document";
+
+const scriptsDemo = "<script id=\"line_chart\" src=\"https://www.gstatic.com/charts/loader.js\"></script><script id=\"line_chart\">var GOOGLE_CHART_LOADED = false;google.charts.load('current', { 'packages': ['barchart', 'piechart', 'linechart'] });google.charts.setOnLoadCallback(scriptLoaded);function  scriptLoaded() {GOOGLE_CHART_LOADED = true;}function drawChart(type = 'PieChart', id = 'piechart', chartData, options) {var data = google.visualization.arrayToDataTable(chartData);var chart = new google.visualization[type](document.getElementById(id));chart.draw(data, options); } function initChart(type, id, chartData, options) {drawChart(type, id, chartData, options)}</script><script id=\"waiting\">function waitForCondition(variable) {function waitFor(result) {if (result) {return result;}return new Promise((resolve) => setTimeout(resolve, 100)).then(() => Promise.resolve(window[variable])).then((res) => waitFor(res));}return waitFor();}</script><script id=\"line_chart\">waitForCondition('GOOGLE_CHART_LOADED').then((res) =>initChart('LineChart','line_chart_S63OEo',[[\"Amount\",\"Sales\",\"Expenses\"],[\"2004\",700,800],[\"2005\",300,900],[\"2006\",200,800],[\"2007\",900,500]],{\"title\":\"Company Performance\",\"display_data\":false,\"curveType\":\"function\",\"legend\":{\"position\":\"none\"},\"colors\":[\"#ff0000\",\"#00ff00\"],\"backgroundColor\":{\"strokeWidth\":0},\"hAxis\":{\"title\":\"Year\",\"minValue\":0,\"textStyle\":{\"bold\":true,\"fontSize\":12,\"color\":\"#4d4d4d\"},\"titleTextStyle\":{\"bold\":true,\"fontSize\":18,\"color\":\"#4d4d4d\"}},\"vAxis\":{\"title\":\"Amount\",\"textStyle\":{\"fontSize\":14,\"bold\":true,\"color\":\"#848484\"},\"titleTextStyle\":{\"fontSize\":14,\"bold\":true,\"color\":\"#848484\"}}}));</script><script id=\"line_chart\">waitForCondition('GOOGLE_CHART_LOADED').then((res) =>initChart('LineChart','line_chart_S63OEo',[[\"Amount\",\"Sales\",\"Expenses\"],[\"2004\",700,800],[\"2005\",300,900],[\"2006\",200,800],[\"2007\",900,500]],{\"title\":\"Company Performance\",\"display_data\":false,\"curveType\":\"function\",\"legend\":{\"position\":\"none\"},\"colors\":[\"#ff0000\",\"#00ff00\"],\"backgroundColor\":{\"strokeWidth\":0},\"hAxis\":{\"title\":\"Year\",\"minValue\":0,\"textStyle\":{\"bold\":true,\"fontSize\":12,\"color\":\"#4d4d4d\"},\"titleTextStyle\":{\"bold\":true,\"fontSize\":18,\"color\":\"#4d4d4d\"}},\"vAxis\":{\"title\":\"Amount\",\"textStyle\":{\"fontSize\":14,\"bold\":true,\"color\":\"#848484\"},\"titleTextStyle\":{\"fontSize\":14,\"bold\":true,\"color\":\"#848484\"}}}));</script>"
+
+const BlogDetailPage = () => {
+
+   const TargetList = {
+      HINDI_JAGRAN: '616a9df6-9ffa-4a13-81d8-c65ea2f92d6b',
+      ENGLISH_JAGRAN: '82d8e272-d6f2-4225-9629-4a4012a43e19',
+      JAGRAN_DEV: 'd5867ee8-34b5-4923-87b4-a7ba35ceaa06'
+   }
+
+   const [demo, setDemo] = useState({ __html: `<div>Loading.....</div>` });
+   const [script, setScript] = useState('')
+   const [style, setStyle] = useState('')
+   const [headLine, setHeadlines] = useState('')
+   const [domLoaded, setDomLoaded] = useState(false);
+   const { slug } = useParams();
+
+   const scriptRef = useRef();
+   const styleRef = useRef();
+   const headlineRef = useRef();
+
+   useEffect(() => {
+      getArticleContent();
+   }, [])
+
+   useEffect(() => {
+      loadScriptToPage();
+   }, [scriptRef, script, styleRef, style, headLine, headlineRef])
+
+   function loadScriptToPage() {
+      if (scriptRef && scriptRef.current) {
+         scriptRef.current.append(document?.createRange()?.createContextualFragment(script))
+      }
+
+      if (styleRef && styleRef.current) {
+         console.log('==========',)
+         styleRef.current.append(document?.createRange()?.createContextualFragment(style))
+      }
+   }
+
+   // useEffect(() => {
+   //    getArticleContent();
+   // }, [domLoaded])
+
+   function createLinks() {
+      return (
+         <span style={{ display: 'flex', gap: '40px' }}>
+            {(() => {
+               let td = [];
+               for (let i = 0; i <= headLine.length; i++) {
+                  td.push(<a href="haryana/mewat-ncr-court-granted-bail-of-congress-mla-maman-khan-in-nuh-violence-case-23546294.html" className="BigTitle_bigTitle__slider__ybWF3" key={i}>{headLine[i]}</a>);
+               }
+               return td;
+            })()}
+         </span>
+      );
+   }
+
+   async function getArticleContent(url) {
+      try {
+         const url = `https://smart-cms-api.jnm.digital/content/api/v1/article/publish/${slug}/content?type=html`
+
+         const headers = {
+            Authorization: "Bearer " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb2RlIjoieVh1V3d0anVVYk43RkQ1ZlZBdzRHeVRNNmNTYzlBIiwidXNlcl9pZCI6IjJhZDJhOGU2LWI3ZTYtNDczYy1hZTM3LTkxM2FhZjAzM2I2MSIsInVzZXJfZW1haWwiOiJxYUB5b3BtYWlsLmNvbSJ9.3pC9tfkPNrQHE1GDYKHVC-gnWViFTyUdALIbS34cV94',
+            'X-Target': TargetList.ENGLISH_JAGRAN
+         }
+
+         const res = await fetch(url, { headers, 'Access-Control-Allow-Origin': '*' })
+         const resJson = await res.json()
+         if (resJson.status == 200) {
+            console.log(resJson.data);
+            setTimeout(() => {
+               setDemo({ __html: resJson.data?.articles?.html });
+               setScript(resJson.data?.articles?.scripts);
+               setStyle(resJson.data?.articles?.styles);
+               setHeadlines(resJson.data?.breakingNews.map(news => news?.art_headline?.en))
+               let inner = document.getElementsByTagName('head')[0].innerHTML;
+               inner += resJson.data?.articles?.styles;
+               document.getElementsByTagName('head')[0].innerHTML = inner;
+            }, 300);
+         } else {
+            console.log("ERROR")
+         }
+      } catch (err) {
+         console.log(err)
+      }
+
+   }
+
+   return demo?.__html?.length > 200 ? (
+
+      <div>
+         {/* <Helmet>
+            <link rel="stylesheet" href="https://unpkg.com/image-compare-viewer/dist/image-compare-viewer.min.css" type="text/css"></link>
+            {script}
+            <script id="pie_chart" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script id="pie_chart"> { `var GOOGLE_CHART_LOADED = false;console.log('=====google=====') google.charts.load('current', { 'packages': ['barchart', 'piechart', 'linechart'] });google.charts.setOnLoadCallback(scriptLoaded);function  scriptLoaded() {GOOGLE_CHART_LOADED = true;}function drawChart(type = 'PieChart', id = 'piechart', chartData, options) {var data = google.visualization.arrayToDataTable(chartData);var chart = new google.visualization[type](document.getElementById(id));chart.draw(data, options); } function initChart(type, id, chartData, options) {drawChart(type, id, chartData, options)}`}</script>
+            <script id="pie_chart1">
+               {
+            `waitForCondition('GOOGLE_CHART_LOADED').then((res) =>initChart('PieChart','pie_chart_5cKGEd',[["Task","Hours Per Day"],["Work",11],["Eat",2],["Watch TV",5],["Sleep",7]],{"title":"Hours of the day","legend":{"position":"none"},"height":500,"backgroundColor":{"strokeWidth":0},"colors":["#ff0000","#0000ff","#00ff00","#9a53d5"]}));`
+            }
+            </script> 
+         </Helmet> */}
+         {headLine?.length ? <div className="BigTitle_bigTitle__XaFRd">
+            <span className="BigTitle_bigTitle__title__rZp9D">
+               <a href="https://www.jagran.com/">बड़ी खबर</a>
+            </span>
+            <div className="BigTitle_bigTitleswiper__dtUew">
+               <div className="swiper swiper-initialized swiper-horizontal swiper-backface-hidden">
+                  <div className="swiper-wrapper">
+                     <div className="swiper-slide BigTitle_bigTitle__slide__KiV_S h3 swiper-slide-active" style={{ marginRight: '50px' }}>
+                        <marquee style={{ display: 'flex', position: 'relative' }}>
+                           {
+                              createLinks()}
+                           {/* <a style={{ display: 'flex', width: '100%'}} href="haryana/mewat-ncr-court-granted-bail-of-congress-mla-maman-khan-in-nuh-violence-case-23546294.html" class="BigTitle_bigTitle__slider__ybWF3">
+                           नूंह हिंसा से जुड़े केस में मामन खान को कोर्ट से बड़ी राहत, कांग्रेस विधायक को मिली जमानत
+                        </a> */}
+                        </marquee>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div> : ''}
+         {/* <marquee style={{ width: '50%', backgroundColor: 'red'}}> asdasdasd sadasdsadas dasdasdasdsa dsadsadsadas dasdsadsad sadsadsadsad sadasdsadsadasdsad sadsadsadsad asdsad sadsa dsadsad</marquee> */}
+         <div dangerouslySetInnerHTML={demo}>
+
+         </div>
+         <div id='scripts' ref={scriptRef}></div>
+      </div>
+   ) : <div style={{ fontSize: '20px' }}>Loading...</div>
+}
+
+export default BlogDetailPage
