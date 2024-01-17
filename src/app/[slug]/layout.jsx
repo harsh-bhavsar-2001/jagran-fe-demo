6501d615ca5eb3e7ed87a989819359dc242b08fc
harsh-18082001 <harsh.bhavsar@saeculumsolutions.com>
2024-01-17 12:52:55 +0530

    some changes

--- src/app/[slug]/layout.jsx
+++ src/app/[slug]/layout.jsx
@@ -68,18 +68,18 @@ export default function BlogDetailLayout({ children }) {
   return (
     <>
       {children}
+      <script src="https://www.gstatic.com/charts/loader.js"></script>
+
+      <link id="font-popins" rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" />
+      <link id="font-inconsolata" rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&amp;display=swap" />
+      <link id="font-mirza" rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Mirza:wght@400;500;600;700&amp;display=swap" />
+      <link id="font-merriweather" rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&amp;display=swap" />
+        
       {/* <header id="mainHeader" style={{ 'zIndex': '999' }}>
-        <script src="https://www.gstatic.com/charts/loader.js"></script>
-
-
-        <link id="font-popins" rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" />
-        <link id="font-inconsolata" rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&amp;display=swap" />
-        <link id="font-mirza" rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Mirza:wght@400;500;600;700&amp;display=swap" />
-        <link id="font-merriweather" rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&amp;display=swap" />
 
         <div className="Header_topStripContainer__XPz4A centered">
           <div className="Header_topStripContainer__language___nE1x"><a href="#">Language</a>
